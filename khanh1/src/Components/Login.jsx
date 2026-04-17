2ef09e245a95b57caf0ce84261ed4fe292fddcb9
tong <maisolokhanh999@gmail.com>
2026-04-17 15:46:28 +0700

    1

--- khanh1/src/Components/Login.jsx
+++ khanh1/src/Components/Login.jsx
@@ -14,7 +14,11 @@ const Login = () => {
       localStorage.setItem('token', 'logged-in');
       localStorage.setItem('user', JSON.stringify(foundUser));
       message.success('Login successful!');
-      navigate("/");
+      if (foundUser.role === 'admin') {
+        navigate("/dashboard"); // Chuyển đến trang quản trị nếu là admin
+      } else {
+        navigate("/"); // Chuyển đến trang chủ nếu là user thường
+      }
     } else {
       message.error('Invalid username or password!');
     }
@@ -23,41 +27,41 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-[calc(100vh-120px)]">
       <Form className='fex items-center justify-center'
-      name="login"
-      initialValues={{ remember: true }}
-      style={{ maxWidth: 360 }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: 'Please input your Username!' }]}
+        name="login"
+        initialValues={{ remember: true }}
+        style={{ maxWidth: 360 }}
+        onFinish={onFinish}
       >
-        <Input prefix={<UserOutlined />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your Password!' }]}
-      >
-        <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-      </Form.Item>
-      <Form.Item>
-        <Flex justify="space-between" align="center">
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-          <a href="#" onClick={(e) => e.preventDefault()}>Forgot password</a>
-        </Flex>
-      </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+        </Form.Item>
+        <Form.Item>
+          <Flex justify="space-between" align="center">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <a href="#" onClick={(e) => e.preventDefault()}>Forgot password</a>
+          </Flex>
+        </Form.Item>
 
-      <Form.Item>
-        <Button block type="primary" htmlType="submit">
-          Log in
-        </Button>
-        <NavLink to="/SignUp">Register now!</NavLink>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+          <NavLink to="/SignUp">Register now!</NavLink>
+        </Form.Item>
+      </Form>
     </div>
-   
+
   );
 };
 
